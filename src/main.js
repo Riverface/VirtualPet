51f76c777848f44e28cfde94c3ea113a90041c4f
Akigawa <sonoskeakigawa@gmail.com>
2019-10-15 11:35:37 -0700

    DONE

--- src/main.js
+++ src/main.js
@@ -9,33 +9,31 @@ $(document).ready(function () {
     let pet = new virtpet();
     pet.money = 1000;
     pet.food = 50;
-    $("#buyfood").click(function(){
-        if(pet.money > pet.foodprice){
-
+    $("#buyfood").click(function () {
+        if (pet.money > pet.foodprice) {
             pet.money -= pet.foodprice;
             pet.foodstock++;
         }
-        $("#feedpet").click(function(){
-            
-            pet.foodstock--;
-            pet.eatflag = true;
-            
+        $("#feedpet").click(function () {
+            if(pet.foodstock >= 1){
+                pet.foodstock--;
+                pet.eatflag = true;
+            }
         });
-
-}); 
-   $("#tickratechanger").click(function(){
-       pet.tickrate = $("#tickrate").val();
+    });
+    $("#tickratechanger").click(function () {
+        pet.tickrate = $("#tickrate").val();
     });
     $("#togglework").click(function () {
         pet.ToggleWork();
     });
-    $("#kill").click(function(){
-    pet.life = 0;
+    $("#kill").click(function () {
+        pet.life = 0;
     });
-    $("#debuglife").click(function(){
-    pet.life = pet.maxlife;
+    $("#debuglife").click(function () {
+        pet.life = pet.maxlife;
     });
-    $("#sleepwake").click(function(){
+    $("#sleepwake").click(function () {
         pet.Togglewake();
     });
     var pagethread = setInterval(() => {
@@ -45,27 +43,24 @@ $(document).ready(function () {
         if (!pet.workflag && !pet.working) {
             $("#workstatus").text("idle");
         }
-        
         if (!pet.workflag && pet.working) {
             $("#workstatus").text("Clocking out at the end of the hour...");
         }
         if (pet.workflag && pet.working) {
             $("#workstatus").text("working...");
-
+        }
+        if (pet.working && pet.sleepflag) {
+            $("#workstatus").text("clocking out after this hour, sprinting home, then going to bed ASAP.")
         }
         if (pet.workflag && !pet.working) {
             $("#workstatus").text("getting ready for work...");
-
         }
         if (pet.workflag) {
             $("#togglework").text("Stop");
-
         }
         if (!pet.workflag) {
             $("#togglework").text("Work");
-
         }
-
 
         if (pet.food == pet.maxfood) {
             $("#feedpet").text("Can't, full");
@@ -77,7 +72,10 @@ $(document).ready(function () {
             $("#sleepwake").text("Sleep");
         } else {
             $("#sleepwake").text("Wake");
-            
+            $("#workstatus").text("Sleeping");
+        }
+        if (pet.sleepflag && !pet.sleeping) {
+            $("#workstatus").text("getting ready to sleep");
         }
         $("#petlife").val(pet.life);
         $("#petlifereadout").text(`${pet.life} / ${pet.maxlife}`);
@@ -89,5 +87,13 @@ $(document).ready(function () {
         $("#petwages").text(`${pet.wages} $ per hour `);
         $("#money").text(`${pet.money}$ in wallet`);
         $("#totaltime").html(pet.time);
+        $("#death").hide();
+
+        if(pet.dead == true){
+            $("#virtualpet").show()
+            $("#death").show();
+            $("#pet").hide();
+            $("#timedead").html(pet.time);
+        }
     }, 10);
 });
--- src/virtpet.js
+++ src/virtpet.js
@@ -6,6 +6,8 @@ export class virtpet {
         this.difficulty = ((difficulty == undefined) ? 1 : difficulty);
         this.name = ((name == undefined) ? "cactuardo" : name);
         this.time = '';
+        this.working = false;
+        this.payday = false;
         this.food = 0;
         this.foodstock = 0;
         this.maxfood = 0;
@@ -13,13 +15,8 @@ export class virtpet {
         this.maxenergy = 0;
         this.strength = 0;
         this.money = 0;
-        this.working = false;
         this.energy = 0;
-        this.foodrate = 1;
-        this.hungerrate = 1;
         this.worktimer = 0;
-        this.payday = false;
-        this.animations = [];
         this.totalticks = 0;
         this.seconds = 0;
         this.minute = 0;
@@ -29,90 +26,97 @@ export class virtpet {
         this.month = 0;
         this.year = 0;
         this.decade = 0;
-        this.foodprice = 1;
-        this.speed = 1;
         this.worktime = 0;
         this.totalworktime = 0;
-        this.wages = 1;
         this.week = 0;
         this.time = 0;
+        this.sleepflag = false;
         this.sleeping = false;
         this.eating = false;
         this.workflag = false;
         this.working = false;
-        this.tickrate = 1;
         this.eatingticks = 0;
+        this.eatframe = false;
+        this.dead = false;
+
+        this.tickrate = 1;
+        this.wages = 1;
+        this.foodprice = 1;
+        this.speed = 1;
+        this.foodrate = 1;
+        this.animations = [];
+        this.hungerrate = 1;
         this.mainthread = setInterval(() => {
-            for(var trate = 0; trate < this.tickrate; trate++){
-            this.Tick();
+            if(!this.dead){
+                for (var trate = 0; trate < this.tickrate; trate++) {
+                    this.Tick();
+                }
             }
         }, 1);
-
     }
-    Tick() {
-        if (this.totalticks == 0) {
-
-            this.FirstParse();
+    SleepChecks() {
+        this.energy = this.sleeping ? this.energy += 5: this.energy-= 1;
+    }
+    EatChecks() {
+        if (this.food == 0) {
+            this.life -= this.hungerrate;
         }
-        this.ParseStats();
-        if(this.eatflag){
-        this.eatingticks = this.totalticks;
-        this.eating = true;
-        }
-
-        this.totalticks++;
-        this.seconds++;
-        if (this.seconds == 60) {
-            if(this.eating){
-                this.eatflag = false;
-                this.food += this.foodrate;
-            if(this.totalticks == this.eatingticks + (60 * 20)){
+        if (this.eating) {
+            this.food += this.foodrate;
+            if (this.totalticks == this.eatingticks + (60 * 20)) {
                 this.eating = false;
             }
-    
+        }
+        if (this.eatflag) {
+            this.eatingticks = this.totalticks;
+            this.eating = true;
+        }
+        if (this.food > this.maxfood) {
+            this.food = this.maxfood;
+            this.eatflag = false;
+        }
+    }
+    Workchecks() {
+        if (this.working) {
+            console.log("Cactuardo is working!");
+            this.worktime++;
+            this.totalworktime++;
+            if (this.energy >= 0) {
+                this.energy -= 2.5;
+            } else {    
+                this.life--;
             }
+        }
+        if (this.workflag == true) {
+            this.working = true;
+        } else if (this.workflag == false) {
+            this.working = false;
+        }
+    }
+    Tick() {
+        (this.totalticks == 0) ? this.FirstParse(): this.ParseStats();
+        this.totalticks++;
+        this.seconds++;
+        this.EatChecks();
+        if (this.seconds == 60) {
             this.minute++;
             this.seconds = 0;
             if (this.minute >= 60) {
+                console.log(this.energy);
                 this.hour++;
                 this.minute = 0;
                 this.food -= this.hungerrate;
                 console.log(this.minute);
-                if (this.working) {
-                    console.log("Cactuardo is working!");
-                    this.worktime++;
-                    this.totalworktime++;
-                    if (this.energy > 0) {
-                        this.energy-= 2.5;
-                    } else {
-                        this.life--;
-                    }
+                if(this.food > 50 && this.energy > 0 ){
+                    this.life++;
                 }
-
-                if(this.sleeping){
-                this.energy += 5;
-                }
-                else{
-                    if (!this.working) {
-                        this.energy--;
-                    }
-                }
-                if (this.workflag == true) {
-
-                    this.working = true;
-
-                } else if (this.workflag == false) {
-                    this.working = false;
-                }
-
-                if (this.food == 0) {
-                    this.life -= this.hungerrate;
-                }
+                this.Workchecks();
+                this.SleepChecks();
 
                 if (this.hour >= 24) {
-
                     this.day++;
                     this.hour = 0;
+
                     if (this.day == 7) {
                         this.money += (this.worktime * this.wages);
                         this.worktime = 0;
@@ -122,9 +126,11 @@ export class virtpet {
                         if (this.week >= 4) {
                             this.month++;
                             this.week = 0;
+
                             if (this.month >= 12) {
                                 this.year++;
                                 this.month = 0;
+
                                 if (this.year == 10) {
                                     this.year = 0;
                                     this.decade++;
@@ -145,17 +151,27 @@ export class virtpet {
         this.energy = this.maxenergy;
         this.food = this.maxfood;
         this.life = this.maxlife;
-        this.foodrate = 1;
+        this.foodrate = 0.25;
         this.hungerrate = 1 * this.difficulty;
+
     }
     ParseStats() {
         this.maxfood = 100 + (parseInt((this.level * 5) * Math.PI) - (parseInt(this.difficulty * 3)));
-        this.maxenergy = 100 + (parseInt((this.level * 3) * Math.PI) - (parseInt(this.difficulty * 3)));
-        this.maxlife = 100 + (parseInt((this.level * 6) * Math.PI) - (parseInt(this.difficulty * 3)));
+        this.maxenergy = 100 + this.strength + (parseInt((this.level * 3) * Math.PI) - (parseInt(this.difficulty * 3)));
+        this.maxlife = 100 + this.strength + (parseInt((this.level * 6) * Math.PI) - (parseInt(this.difficulty * 3)));
         this.strength = ((this.level * 1) + parseInt(this.totalworktime / Math.PI));
         this.wages = (parseFloat((this.strength * this.speed) * .05).toPrecision(1));
         this.time = `${this.decade} decades,</br> ${this.year} years,</br> ${this.month} months,</br> ${this.week} weeks, ${this.day} days, </br> ${this.hour} hours,</br> ${this.minute} minutes,</br> ${this.seconds} seconds, </br> ${this.totalticks} ticks`;
         this.foodprice = (this.difficulty * (this.foodstock / 2));
+        if(this.energy > this.maxenergy){
+            this.energy = this.maxenergy;
+        }
+        if(this.life > this.maxlife){
+            this.life = this.maxlife;
+        }
+        if(this.life <= 0){
+        this.dead = true;
+        }
     }
     FillEnergy() {
         this.energy = this.maxenergy;
@@ -163,14 +179,15 @@ export class virtpet {
     FillHunger() {
         this.food = this.maxfood;
     }
-    Feed() {
-        if (this.foodstock > 0) {
-            this.hunger += this.foodrate;
-            this.foodstock--;
-        }
+    FillLife(){
+        this.life = this.maxlife;
+        this.dead = false;
     }
-
     Togglewake() {
+        if (this.working) {
+            this.workflag = false;
+            this.sleepflag = true;
+        }
         if (!this.sleeping) {
             this.sleeping = true;
         } else {
